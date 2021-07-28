769efc78e593a222b36cdcdf353b6fa3cdde5de1
Kenia Cordero <Kenya.corvaz99@gmail.com>
2021-07-27 23:14:05 -0600

    Corregidos los Endpoinds

--- servidor/src/controllers/controller.js
+++ servidor/src/controllers/controller.js
@@ -16,6 +16,8 @@ function listarById (req, res) {
             }
         });
     }
+
+
 }
 
 function actualizar (req, res){
@@ -67,9 +69,103 @@ function eliminarFavoritos (req, res){
     }
 }
 
+function listar (req, res) {
+    if(connection){
+        let sql = "select b.id, b.name, b.publisher, b.category, b.description, b.year, f.idBoardgame from boardgames b left join favorites f on f.idBoardgame = b.id";
+        connection.query(sql, (err, data) => {
+            if(err){
+                res.status(400).json(err);
+            }else{
+                let favorite = false;
+                if(data.idBoardgame != null){
+                    favorite = true;
+                }
+                res.json({result: data, favorite});
+            }
+        });
+    }	
+}
+
+function crear(req, res){
+    if(connection){
+        const boardgame = req.body;
+        if(!boardgame.Name){
+            return res.status(400).send({error:true, mensaje:"El nombre es obligatorio"})
+        }
+        if(!boardgame.publisher){
+            return res.status(400).send({error:true, mensaje:"El editor es obligatorio"})
+        }
+        if(!boardgame.category){
+            return res.status(400).send({error:true, mensaje:"La categoria es obligatoria"})
+        }
+        if(boardgame.Name.length > 80 && Boardgame.Name){
+            return res.status(400).send({error:true, mensaje:"El nombre no debe tener mas de 80 caracteres"})
+        }
+        if(boardgame.Publisher.length > 60 && Boardgame.publisher){
+            return res.status(400).send({error:true, mensaje:"El editor no debe tener mas de 60 caracteres"})
+        }
+        if(boardgame.Category.length != 2 && Boardgame.category){
+            return res.status(400).send({error:true, mensaje:"La debe tener dos caracteres"})
+        }
+        if(boardgame.Description.length > 200){
+            return res.status(400).send({error:true, mensaje:"La descripcion no debe tener mas de 200 caracteres"})
+        }
+        if(boardgame.Year.length > 4){
+            return res.status(400).send({error:true, mensaje:"El año debe tener 4 caracteres"})
+        }
+
+        let sql ="insert into boardgames set ?";
+
+        connection.query(sql,[boardgame], (err, data) => {
+            if(err){
+                res.status(400).json(err);
+            } else {
+                res.json({error:false,resultado: data, mensaje:"Boardgame creado con exito"});
+            }
+        });
+    }
+}
+
+function listarfav(req, res){
+    if(connection){
+        let sql = "select b.name, b.publisher, b.year, f.idBoardgame from boardgames b left join favorites f on f.idBoardgame = b.id";
+        connection.query(sql, (err, data) => {
+            if(err){
+                res.status(400).json(err);
+            }else{
+                if(data.idBoardgame != null){
+                    res.json({result: data, favorite});
+                }
+                
+            }
+        });
+    }
+}
+
+function crearfav(req,res){
+    if(connection){
+        const favorites = req.body;
+
+        let sql ="insert into favorites set ? where id = ?";
+
+        connection.query(sql,[Boardgame, id], (err, data) => {
+            if(err){
+                res.status(400).json(err);
+            } else {
+                res.json({error:false,resultado: data, mensaje:"Boardgame agregado a favoritos"});
+            }
+        });
+    }
+    
+}
+
 module.exports = {
     listarById,
     actualizar,
     eliminarBoardgame,
     eliminarFavoritos,
+    listar,
+    crear,
+    listarfav,
+    crearfav,
 };
--- servidor/src/routes/routes.js
+++ servidor/src/routes/routes.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const { crearfav } = require('../controllers/controller');
 
 const routes = express.Router();
 
-const {listarById, actualizar, eliminarBoardgame, eliminarFavoritos,listar,crear,listarfav} = require('../controllers/controller');
+const {listarById, actualizar, eliminarBoardgame, eliminarFavoritos,listar,crear,listarfav,crearfav} = require('../controllers/controller');
 
 //Creación de las rutas
 routes.get("/boardgame/:id", listarById);
 routes.get("/boardgame",listar);
 routes.post("/boardgame", crear);
 routes.get("/favorites",listarfav);
-routes.post("/favorites",cre);
+routes.post("/favorites",crearfav);
 routes.put("/boardgame/:id", actualizar);
 routes.delete("/boardgame/:id", eliminarBoardgame);
 routes.delete("/favorites/:id", eliminarFavoritos);
