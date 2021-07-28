432d588e08fa3ef2e01c8648c259e09c3d9e8dac
Kenia Cordero <Kenya.corvaz99@gmail.com>
2021-07-27 22:35:36 -0600

    hice cambios

--- servidor/src/routes/routes.js
+++ servidor/src/routes/routes.js
@@ -2,4 +2,12 @@ const express = require('express');
 
 const routes = express.Router();
 
+const {listarById,listar,crear,listarfav, crearfav} = require('../controllers/controller');
+
 //Creación de las rutas
+routes.get("/boardgame/:id", listarById);
+routes.get("/boardgame",listar);
+routes.post("/boardgame", crear);
+routes.get("/favorites",listarfav);
+routes.post("/favorites",crearfav);
+module.exports = routes;
